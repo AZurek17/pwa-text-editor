9077300791de36b7484bad8a7bdb8a52a5f791eb
Andy <84168944+AZurek17@users.noreply.github.com>
2023-11-29 10:36:06 -0700

    updated install.js, and src.swjs

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -4,7 +4,7 @@ const butInstall = document.getElementById('buttonInstall');
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
     window.deferredPrompt = event;
-    butInstall.removeAttribute('hidden');
+    butInstall.removeAttribute('hidden', false);
 
 });
 
